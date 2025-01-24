036fe2a242c44a72d9388874e033b2567e75fa98
yasmeen Alaa Taha <138386021+Yasmeen34320@users.noreply.github.com>
2025-01-25 01:40:53 +0200

    make_courses_dynamic

--- DBProject/DBProject/Program.cs
+++ DBProject/DBProject/Program.cs
@@ -11,7 +11,7 @@ namespace DBProject
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Student_Courses(3));
+            Application.Run(new LogIn());
         }
     }
 }
