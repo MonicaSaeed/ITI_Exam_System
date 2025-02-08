de7403483681ff2d024c3a7b0b8865b4410a8b11
yasmeen Alaa Taha <138386021+Yasmeen34320@users.noreply.github.com>
2025-02-08 14:22:16 +0200

    Edit_totalGrade

--- DBProject/DBProject/ExamResultsForm.cs
+++ DBProject/DBProject/ExamResultsForm.cs
@@ -13,13 +13,14 @@ namespace DBProject
     public partial class ExamResultsForm : Form
     {
         private List<(string Question, List<string> Options, List<string> StudentAnswers, List<string> CorrectAnswers, int QuestionGrade)> results;
-        private int totalGrade;
+        private int totalGrade , actualTotal;
         string connectionString = "Server=localhost\\SQLEXPRESS;Database=ExaminationSystem;Integrated Security=True;TrustServerCertificate=True;";
 
-        public ExamResultsForm(int totalGrade, List<(string Question, List<string> Options, List<string> StudentAnswers, List<string> CorrectAnswers, int QuestionGrade)> results1)
+        public ExamResultsForm(int totalGrade, List<(string Question, List<string> Options, List<string> StudentAnswers, List<string> CorrectAnswers, int QuestionGrade)> results1,int actual)
         {
             this.results = results1;
             this.totalGrade = totalGrade;
+            this.actualTotal = actual;
             InitializeComponent();
             
             //LoadResults();
